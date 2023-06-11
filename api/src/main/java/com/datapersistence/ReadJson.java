e5992f381029c99e76b8879865ab544beb4b712c
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2023-06-11 14:23:59 -0300

    ...

--- api/src/main/java/com/datapersistence/ReadJson.java
+++ api/src/main/java/com/datapersistence/ReadJson.java
@@ -2,13 +2,18 @@ package com.datapersistence;
 
 import javax.json.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 
 class Exemplo implements JsonSerializable{
+    private UUID id = UUID.randomUUID();
     private String nome;
     private String email;
     private List<String> materias;
@@ -25,8 +30,14 @@ class Exemplo implements JsonSerializable{
         getMaterias().forEach(jsonArrayBuilder::add);
         return Json.createObjectBuilder()
                 .add("nome", getNome())
+                .add("id", getId().toString())
                 .add("email", getEmail())
                 .add("materias", jsonArrayBuilder).build();
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
     }
 
     public String getNome() {
@@ -61,49 +72,52 @@ public class ReadJson {
             materias.add("mc358");
             materias.add("ma311");
             Exemplo exemplo = new Exemplo("Eliel Oliveira da Silva", "eliel.oliveira2004@gmail.com", materias);
-            writeFile("data/exemplo.json", exemplo);
+//            writeFile(exemplo);
+            System.out.println(readFile("dados/Exemplo/b4873914-7a66-4255-bee1-fbe097c78fab.json"));
 //            readFile("data/exemplo.json");
         } catch (Exception e){
             e.printStackTrace();
         }
     }
-    public static JsonArray readFile(String path) throws Exception{
+    public static JsonObject readFile(String path) throws NullPointerException, FileNotFoundException {
         File file = new File(path);
-        if (file.createNewFile()){
-            JsonArray emptyJsonArray = Json.createArrayBuilder().build();
-            writeFile(path, emptyJsonArray);
-        }
         JsonReader jsonReader = Json.createReader(new FileReader(file));
-        JsonArray jsonArray = jsonReader.readArray();
+        JsonObject jsonObject = jsonReader.readObject();
         jsonReader.close();
-        return jsonArray;
+        return jsonObject;
     }
 
-    public static void writeFile(String path, JsonArray jsonArray) {
+    public static void writeFile(String path, JsonObject jsonObject) {
+        createFolderIfNotExists(path);
         try (FileWriter fileWriter = new FileWriter(path)){
             JsonWriter jsonWriter = Json.createWriter(fileWriter);
-            jsonWriter.writeArray(jsonArray);
+            jsonWriter.writeObject(jsonObject);
             jsonWriter.close();
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    public static void writeFile(String path, JsonSerializable jsonSerializable) throws Exception {
-        JsonArray jsonArray = readFile(path);
-        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-        jsonArray.forEach(arrayBuilder::add);
-        arrayBuilder.add(jsonSerializable.writeJson());
-        writeFile(path, arrayBuilder.build());
+    public static void writeFile(JsonSerializable jsonSerializable) throws Exception {
+        String path = "dados/" + jsonSerializable.getClass().getSimpleName() + "/" + jsonSerializable.getId() + ".json";
+        writeFile(path, jsonSerializable.writeJson());
+    }
 
-//        try (FileWriter fileWriter = new FileWriter(path)){
-//
-//            JsonWriter jsonWriter = Json.createWriter(fileWriter);
-////            jsonArray.add(jsonSerializable.writeJson());
-////            jsonWriter.writeArray(jsonArray);
-//            jsonWriter.close();
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
+    private static String getPath(JsonSerializable jsonSerializable){
+        return "dados/" + jsonSerializable.getClass().getSimpleName() + "/" + jsonSerializable.getId() + ".json";
+    }
+
+    private static void createFolderIfNotExists(String path){
+        String folderPath = getParentPath(path);
+        new File(folderPath).mkdirs();
+    }
+
+    private static String getParentPath(String path){
+        Path path1 = Paths.get(path);
+        Path folderPath = path1.getParent();
+        if (folderPath != null){
+            return folderPath.toString();
+        }
+        return "";
     }
 }
