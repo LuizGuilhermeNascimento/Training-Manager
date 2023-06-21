accdca198451396d97b538c2c3c211772c4a28b6
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2023-06-21 14:20:37 -0300

    ...

--- api/src/main/java/com/projeto_mc322/api/dtos/LoginSignupResponseDTO.java
+++ api/src/main/java/com/projeto_mc322/api/dtos/LoginSignupResponseDTO.java
@@ -6,23 +6,23 @@ import com.projeto_mc322.api.models.user.User;
 import java.util.UUID;
 
 public class LoginSignupResponseDTO {
-    private String role;
+    private Integer role; // professor = 0, aluno = 1
     private UUID id;
 
     public LoginSignupResponseDTO(User user) {
         if (user instanceof Professor){
-            this.role = "professor";
+            this.role = 0;
         }else{
-            this.role = "aluno";
+            this.role = 1;
         }
         this.id = user.getId();
     }
 
-    public String getRole() {
+    public Integer getRole() {
         return role;
     }
 
-    public void setRole(String role) {
+    public void setRole(Integer role) {
         this.role = role;
     }
 
