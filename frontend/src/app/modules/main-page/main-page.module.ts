06cb150b0a487b257840b5942f27a13d5e25af2f
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2023-06-22 11:23:27 -0300

    ...

--- frontend/src/app/modules/main-page/main-page.module.ts
+++ frontend/src/app/modules/main-page/main-page.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProfessorPageModule } from './professor-page/professor-page.module';
-import { ProfessorPageComponent } from './professor-page/professor-page/professor-page.component';
-
-
 
 @NgModule({
   declarations: [
-    ProfessorPageComponent,
-    MainPageComponent
   ],
   imports: [
-    NgModule,
     CommonModule,
-    ProfessorPageModule
   ]
 })
 export class MainPageModule { }
