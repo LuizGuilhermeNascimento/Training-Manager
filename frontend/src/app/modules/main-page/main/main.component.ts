08a0952026cbbc98fd13cc05ff5c099da62d4ead
Luiz Guilherme Nascimento <l230667@dac.unicamp.br>
2023-06-23 16:31:28 -0300

    conclusão do register

--- frontend/src/app/modules/main-page/main/main.component.ts
+++ frontend/src/app/modules/main-page/main/main.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserJson } from 'src/app/models/login.models';
+import { keys } from 'src/app/services/local-storage/keys.json';
+import { LocalStorageService } from 'src/app/services/local-storage/localstorage.service';
 
 @Component({
   selector: 'app-main',
@@ -7,4 +11,13 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
 
+  user: UserJson;
+  constructor(private router: Router, private localStorageService: LocalStorageService) {
+    this.user = {
+      id: this.localStorageService.getItem<string>(keys.idKey) ?? '',
+      role: this.localStorageService.getItem<number>(keys.roleKey) ?? 0
+    }
+  }
+
+  
 }
