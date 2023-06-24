65490235b9f49518b4cd2a6fc8ed5a18c89517ae
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2023-06-24 12:14:15 -0300

    adicionando mensagem de cpf/email/cref já cadastrado no registrar

--- api/src/main/java/com/projeto_mc322/api/repositories/AcompanhamentoRepository.java
+++ api/src/main/java/com/projeto_mc322/api/repositories/AcompanhamentoRepository.java
@@ -1,14 +1,7 @@
 package com.projeto_mc322.api.repositories;
 
-import com.datapersistence.BuildObject;
-import com.datapersistence.JsonManager;
-import com.projeto_mc322.api.exceptions.HttpException;
 import com.projeto_mc322.api.models.acompanhamento.Acompanhamento;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
-
-import javax.json.JsonObject;
-import java.util.UUID;
 
 @Repository
 public class AcompanhamentoRepository extends RepositoryBase<Acompanhamento> {
